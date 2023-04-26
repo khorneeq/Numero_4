d7b9d9396577411dc19073a37348e0e0aa3f9839
khorneeq <130909083+khorneeq@users.noreply.github.com>
2023-04-26 14:13:28 +0200

    Zadanie numero 4

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Numero_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = 199;
+            Console.WriteLine(CloseEnough(x));
+        }
+        public static bool CloseEnough(int x)
+        {
+            return Math.Abs(100 - x) <= 10 || Math.Abs(200 - x) <= 100;
+        }
+    }
+}
